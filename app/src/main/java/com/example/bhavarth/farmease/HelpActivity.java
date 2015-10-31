3d451683e5c54c7ed244e23111c6c24c9b3fdcf6
BhavarthChauhan <bhavarth.chauhan95@gmail.com>
2015-11-01 01:40:15 +0530

    First commit

---
+++ app/src/main/java/com/example/bhavarth/farmease/HelpActivity.java
@@ -0,0 +1,9 @@
+package com.example.bhavarth.farmease;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by bhavarth on 1/11/15.
+ */
+public class HelpActivity extends AppCompatActivity {
+}
